847aa4e5d85b387eba4b06c9c8038ff9cfa2325e
Meharjot Kaur <meharjotkaur07@gmail.com>
2025-02-16 06:57:43 -0800

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -20,7 +20,7 @@ int main() {
         case '/':
             if (num2 != 0) {
                 
-                printf("%d / %d = %.2f\n", num1, num2, (float)num1 / num2);
+                printf("%d %d / = %d\n", num1, num2, num1 / num2);
             } else {
                 printf("Error: Division by zero\n");
             }
