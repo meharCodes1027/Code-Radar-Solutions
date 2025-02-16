34aa2a07804b0ec0c0db5f3665f02ba9d0bb809e
Meharjot Kaur <meharjotkaur07@gmail.com>
2025-02-16 08:54:56 -0800

    Solution to Perform Bitwise Right Shift

---
+++ Perform Bitwise Right Shift@@67882a731b4392d8fc1577e6/code.c
@@ -0,0 +1,12 @@
+// Your code here...
+#include <stdio.h>
+
+int main() {
+    int m, n, result;
+    scanf("%d %d", &m, &n);
+    result = (m >> n); 
+    
+    printf("%d\n", result); 
+    
+    return 0;
+}
